4f997183f05b3f8fa62decf7d73e5662ecbbcc4b
Joao Rangel <joao.vran@gmail.com>
2021-01-12 18:55:00 -0300

    update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,6 +9,11 @@
         Cadastro
       </router-link>
     </nav>
+    <div id="title">
+      <div>
+        <h1>Cadastro</h1>
+      </div>
+    </div>
   </header>
 </template>
 
@@ -41,5 +46,22 @@
     font-size: 12px;
     margin: 0 16px 2px 54px;
     vertical-align: middle;
+  }
+
+  #title {
+    background: #EFF4F9;
+    display: flex;
+    justify-content: center;
+  }
+
+  #title div {
+    width: 100%;
+    max-width: 720px;
+    padding: 20px 0;
+  }
+
+  #title h1 {
+    font: 80px "Noto Serif", arial, sans-serif;
+    font-weight: 400;
   }
 </style>
