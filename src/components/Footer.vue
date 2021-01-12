6a816d4811e001ba84e0aa229f6dc238dab8040d
Joao Rangel <joao.vran@gmail.com>
2021-01-12 14:52:27 -0300

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <p>
+      Seu Nome - 2020
+    </p>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'Footer',
+  }
+</script>
+
+<style scoped>
+  footer {
+    width: 100%;
+    background: #1188EE;
+  }
+
+  p {
+    padding: 60px 60px;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    text-decoration: none;
+  }
+</style>
